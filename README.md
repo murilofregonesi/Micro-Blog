e1385182a7af8bc1d2e14625f2f31afb2f9e1201
Murilo Fregonesi Falleiros <murilo.fregonesi@gmail.com>
2021-03-04 20:52:53 -0300

    Heroku deployment

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Micro-Blog
 Micro-Blog with Flask and MongoDB
 
-> *mongo_connection_string* is missing from the files for safety reasons. It only keeps my personal **MongoDB connection string**.
-> Check out the official [Atlas website](https://docs.atlas.mongodb.com/connect-to-cluster/) to know how to connect the application to your own cluster.
+> *.env* is not included for safety reasons. It keeps **MongoDB connection string**.
+> Check out [Atlas website](https://docs.atlas.mongodb.com/connect-to-cluster/) and **.env.example** to understand how to connect the application to your own cluster.
--- app.py
+++ app.py
@@ -3,22 +3,27 @@ from pymongo import MongoClient
 from datetime import datetime
 from mongo_connection_string import connection_string
 
-app = Flask(__name__)
+from dotenv import load_dotenv
+import os
 
-client = MongoClient(connection_string)
-app.db = client.microblog
-entries = app.db.entries
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/home/', methods=["GET", "POST"])
-def home():
-    if request.method == 'POST':
-        content = request.form.get('content')
-        if content:
-            today = datetime.today().date()
-            entries.insert({"content": content, "date": today.strftime("%Y-%m-%d")})
-    
-    entries_tuples = [(entry["content"], 
-                       entry["date"], 
-                       datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")) for entry in entries.find({})]
+    client = MongoClient(os.environ.get("MONGODB_URI"))
+    app.db = client.microblog
+    entries = app.db.entries
 
-    return render_template('home.html', entries=entries_tuples)
+    @app.route('/home/', methods=["GET", "POST"])
+    def home():
+        if request.method == 'POST':
+            content = request.form.get('content')
+            if content:
+                today = datetime.today().date()
+                entries.insert({"content": content, "date": today.strftime("%Y-%m-%d")})
+        
+        entries_list = [(entry["content"], 
+                        entry["date"], 
+                        datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")) for entry in entries.find({})]
+
+        return render_template('home.html', entries=entries_list)
+    return app
